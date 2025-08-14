8b399ba1835fc111bc70641fbac3ee21bc01dc0b
SAIMANIKESH <saimanikesh2004@gmail.com>
2025-08-14 20:42:08 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export default function App() {
+  return (
+    <div className='flex-center h-[100vh]'>
+      Mojito
+    </div>
+  )
+}
